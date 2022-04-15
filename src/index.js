d900183d524301bf9969914738fe62886f7aedbe
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-15 11:46:43 -0400

    Final commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ let characterVotes = document.querySelector('#vote-count')
 fetch(url)
 //convert that data into javascript so we can work with it
 .then(res => res.json())
-//iterate through each character and add to the character bar
-.then(characters => characters.forEach(character => addToCharacterBar(character)))
+.then(characters => {
+    //when you refresh, set the characterInfo to first character
+    const name = characterInfo.querySelector('#name')
+    const image = characterInfo.querySelector('#image')
+    name.textContent = characters[0].name
+    image.src = characters[0].image
+    characterVotes.textContent = 0
+
+    //iterate through each character and add to the character bar
+    characters.forEach(character => addToCharacterBar(character))
+})
 //catch any errors and print in console
 .catch(err => console.log(err))
 
@@ -29,10 +38,8 @@ const addToCharacterBar = (character) => {
         name.textContent = character.name
         image.src = character.image
 
-        //set the charactervotes to 0
-        characterVotes.textContent = 0
+        characterVotes.textContent = character.votes
     })
-    
 
 }
 
@@ -48,7 +55,82 @@ form.addEventListener('submit', (e) => {
     let addedVotes = parseInt(e.target.votes.value, 10)
     //add the inputed vote to the current votes and input that number into the textContent
     characterVotes.textContent = (currentVotes += addedVotes)
+    console.log(characterVotes.textContent)
     form.reset()
+
+    //Use PATCH to reset character votes in json file to adjusted votes^^
+    fetch(url)
+    .then(res => res.json())
+    .then(characters => {
+        const charName = document.querySelector('#name')
+        const charID = characters.find(character => character.name === charName.textContent)
+        console.log(charID)
+        fetch(`${url}/${charID.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                votes : characterVotes.textContent
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    })
+})
+
+
+
+//reset button is a little buggy
+const resetButton = document.querySelector('#reset-btn')
+
+resetButton.addEventListener('click', (e) => {
+
+    fetch(url)
+    .then(res => res.json())
+    .then(characters => {
+        const charName = document.querySelector('#name')
+        const charID = characters.find(character => character.name === charName.textContent)
+        console.log(charID)
+        fetch(`${url}/${charID.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                votes : '0'
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    })
+
+    characterVotes.textContent = character.votes
+})
+
+
+//Add new character using POST method
+const addNewForm = document.querySelector("#character-form")
+
+addNewForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newName = e.target.name.value
+    const newImage = e.target['image-url'].value
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            name : newName,
+            image : newImage,
+            votes : '0',
+        })
+    })
+    .then(res => res.json())
+    .then(data => addToCharacterBar(data))
+    .catch(err => console.log(err))
 })
 
 
